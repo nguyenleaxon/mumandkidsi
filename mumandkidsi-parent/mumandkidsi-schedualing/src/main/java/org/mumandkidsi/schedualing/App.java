da4eb7fdf5bb414e23960b506626edcc5b7d9a89
nguyenlesmartdev <nguyen.levu@smartdev.vn>
2015-08-25 15:33:07 +0700

    build structure

---
+++ mumandkidsi-parent/mumandkidsi-schedualing/src/main/java/org/mumandkidsi/schedualing/App.java
@@ -0,0 +1,13 @@
+package org.mumandkidsi.schedualing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
